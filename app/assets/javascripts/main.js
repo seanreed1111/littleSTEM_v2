db22267317ad23bc50a932953358a6cc55592dd2
seanreed1111 <seanreed1111@gmail.com>
2017-02-02 15:57:00 -0500

    fixes moon.html.erb

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,152 +1,31 @@
 
 function main() {
+    (function () {
+       'use strict';
 
-(function () {
-   'use strict';
-
-   /* ==============================================
-  	Testimonial Slider
-
-
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
-        }
-      });
-    =============================================== */ 
-    /*====================================
-    Show Menu
-    ======================================*/
-    $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 500;
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
-    });
-
-    $('body').scrollspy({ 
-        target: '.navbar-default',
-        offset: 80
-    })
-
-    /*====================================
-    Owl Carousel Menu
-
-  	$(document).ready(function() {
-  	  $("#team").owlCarousel({
-  	 
-  	      navigation : false, // Show next and prev buttons
-  	      slideSpeed : 300,
-  	      paginationSpeed : 400,
-  	      autoHeight : true,
-  	      itemsCustom : [
-				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 4],
-				        [1400, 4],
-				        [1600, 4]
-				      ],
-  	  });
-
-  	  $("#clients").owlCarousel({
-  	 
-  	      navigation : false, // Show next and prev buttons
-  	      slideSpeed : 300,
-  	      paginationSpeed : 400,
-  	      autoHeight : true,
-  	      itemsCustom : [
-				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 5],
-				        [1400, 5],
-				        [1600, 5]
-				      ],
-  	  });
-
-      $("#testimonial").owlCarousel({
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-        });
-
-  	});
-    ======================================*/
-  	/*====================================
-    Portfolio Isotope Filter
-
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
+        $(window).bind('scroll', function() {
+            var navHeight = $(window).height() - 500;
+            if ($(window).scrollTop() > navHeight) {
+                $('.navbar-default').addClass('on');
+            } else {
+                $('.navbar-default').removeClass('on');
             }
         });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
 
-    });
-    ======================================*/
-    /*
-      Description:
-      The toLocaleDateString() method returns a string with a language sensitive representation of the date portion of this date. The new locales and options arguments let applications specify the language whose formatting conventions should be used and allow to customize the behavior of the function. In older implementations, which ignore the locales and options arguments, the locale used and the form of the string returned are entirely implementation dependent.
-    
-      Syntax:
-      dateObj.toLocaleDateString([locales [, options]])
-    */
-  	/*====================================
-    CounterUp
-    ======================================*/	
+        $('body').scrollspy({
+            target: '.navbar-default',
+            offset: 80
+        })
 
-	$(document).ready(function( $ ) {
-		if($("span.count").length > 0){	
-			$('span.count').counterUp({
-					delay: 10, // the delay time in ms
-			time: 1000 // the speed time in ms
-			});
-		}
-	});
-	
-	/*====================================
-    WOW JS
+    	$(document).ready(function( $ ) {
+    		if($("span.count").length > 0){
+    			$('span.count').counterUp({
+    					delay: 10, // the delay time in ms
+    			time: 1000 // the speed time in ms
+    			});
+    		}
+    	});
 
-
-	new WOW().init();
-	//smoothScroll
-	smoothScroll.init();
-======================================*/
-}());
-  
-
+    }());
 }
 main();
